9ddfc6d98523e2811656be3217d8b523ca9e2775
sakshijain <sakshi903jain@gmail.com>
2023-05-07 09:37:58 +0530

    by using 2 pointers

--- sliding_window/maximum_sum_subarray.py
+++ sliding_window/maximum_sum_subarray.py
@@ -46,3 +46,37 @@ k = int(input())
 arr = [int(x)for x in input().split()]
 print(sliding_window(arr,k))
 
+
+
+
+
+
+
+# silding window tc = O(N) (by taking 2 pointers)
+# increment j pointer until window size is reached.Once window size is reached then increment i and j to maintain the window size and to subtract the first element of the last window and add the last element of the current window
+
+# def sliding_window(arr,k):
+
+#     n = len(arr)
+#     if n<k:
+#         return "invalid input"
+#     window_sum = 0
+#     max_sum = float('-inf')
+#     i,j=0,0
+#     while j < n:
+#         if j - i != k:
+#             window_sum += arr[j]
+#             j += 1
+#         else:
+#             if window_sum > max_sum:
+#                 max_sum = window_sum
+#             window_sum = window_sum - arr[i] + arr[j]
+#             j += 1
+#             i += 1
+#     if window_sum > max_sum:
+#         max_sum = window_sum
+#     return max_sum
+
+# k = int(input())
+# arr = [int(x)for x in input().split()]
+# print(sliding_window(arr,k))
